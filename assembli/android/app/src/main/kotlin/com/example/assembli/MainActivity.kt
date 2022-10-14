5d4f0e930b674625b7033c8542bdb84eaed95f1a
briaokwor <briaokwor@outlook.com>
2022-10-14 15:00:24 -0500

    draftUI
    Follows tutorial from YT

---
+++ assembli/android/app/src/main/kotlin/com/example/assembli/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assembli
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ assembli/lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:assembli/learn_flutter_page.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const LearnFlutterPage();
+              },
+            ),
+          );
+        },
+        child: const Text('Learn Flutter'),
+      ),
+    );
+  }
+}
