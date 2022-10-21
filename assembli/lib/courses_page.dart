fc1a62afa882601328a5a3fe8860af9e0d568845
briaokwor <briaokwor@outlook.com>
2022-10-21 13:14:11 -0500

    Documented aka updates
    added documentation

--- assembli/lib/courses_page.dart
+++ assembli/lib/courses_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+//class holds basis for courses destination; 
+//will work similar to landing, linking other pages/states 
+//(linked from navigationBar destination in landing.dart) 
+
 class CoursesPage extends StatelessWidget {
 const CoursesPage({Key? key }) : super(key: key);
 
--- assembli/lib/profile_page.dart
+++ assembli/lib/profile_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+//class holds basis for profile destination; 
+//will work similar to landing, linking other pages/states 
+//(linked from navigationBar destination in landing.dart) 
+
 class ProfilePage extends StatelessWidget {
 const ProfilePage({ Key? key }) : super(key: key);
 
