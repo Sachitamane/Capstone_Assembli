033ced9a42eace8570c17e1de6d463aabc0f4c27
Sachitamane <82063804+Sachitamane@users.noreply.github.com>
2022-10-23 20:25:26 -0500

    added listview
    can view classes in a list ( currently hardcoded)

--- assembli/lib/landing.dart
+++ assembli/lib/landing.dart
@@ -13,59 +13,52 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-  //im a little fuzzy on how this var is set, but current page is used in 
+  //im a little fuzzy on how this var is set, but current page is used in
   //setting the index value in selectedIndex: (see line 41 and line 47) aka setState()
   int currentPage = 1;
 
-  //list is used for the bottom navigation bar 
-  List pages = const [
-    AnnouncementsPage(),  
-    CoursesPage(),  
-    ProfilePage()
-  ];
+  //list is used for the bottom navigation bar
+  List pages = const [AnnouncementsPage(), CoursesPage(), ProfilePage()];
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 179, 194, 168),
         title: const Text('Assembli'),
       ),
       body: pages[currentPage],
-      bottomNavigationBar: NavigationBarTheme( 
+      bottomNavigationBar: NavigationBarTheme(
         data: const NavigationBarThemeData(
           indicatorColor: Color.fromARGB(255, 133, 151, 118),
         ),
         child: NavigationBar(
           height: 70,
           selectedIndex: currentPage,
-          backgroundColor:  const Color.fromARGB(255, 179, 194, 168),
+          backgroundColor: const Color.fromARGB(255, 179, 194, 168),
           labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-          
           onDestinationSelected: (int index) {
             setState(() {
               currentPage = index;
             });
             debugPrint("dest select");
           },
-
           destinations: const [
             NavigationDestination(
-              selectedIcon: Icon(Icons.article),
-              icon: Icon(Icons.article_outlined),
-              label: 'Announcements'),
+                selectedIcon: Icon(Icons.article),
+                icon: Icon(Icons.article_outlined),
+                label: 'Announcements'),
             NavigationDestination(
-              selectedIcon: Icon(Icons.fact_check),
-              icon: Icon(Icons.fact_check_outlined),
-              label: 'Courses'),
+                selectedIcon: Icon(Icons.fact_check),
+                icon: Icon(Icons.fact_check_outlined),
+                label: 'Courses'),
             NavigationDestination(
-              selectedIcon: Icon(Icons.person),
-              icon: Icon(Icons.person_outlined),
-              label: 'Profile')
+                selectedIcon: Icon(Icons.person),
+                icon: Icon(Icons.person_outlined),
+                label: 'Profile')
           ],
         ),
       ),
     );
   }
-  
 }
