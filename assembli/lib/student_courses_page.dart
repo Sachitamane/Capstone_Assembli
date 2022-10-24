20f0361d3804a0e45b01e72baeba647da2698e27
briaokwor <briaokwor@outlook.com>
2022-10-24 18:25:38 -0500

    Revert "added listview for student_courses_page"
    This reverts commit 8bd439a36fe49c91919461f8c3910a04f93000bf.

--- assembli/lib/student_courses_page.dart
+++ assembli/lib/student_courses_page.dart
@@ -7,37 +7,10 @@ import 'package:flutter/material.dart';
 class StudentCoursesPage extends StatelessWidget {
   const StudentCoursesPage({Key? key}) : super(key: key);
 
-  //@override
-  //Widget build(BuildContext context) {
-  //return const Center(
-  //  child: Text("Courses Page"),
-  //);
-  //}
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "ListView.builder",
-        theme: ThemeData(primarySwatch: Colors.green),
-        debugShowCheckedModeBanner: false,
-        // home : new ListViewBuilder(),  NO Need To Use Unnecessary New Keyword
-        home: const ListViewBuilder());
-  }
-}
-
-class ListViewBuilder extends StatelessWidget {
-  const ListViewBuilder({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                leading: const Icon(Icons.account_circle),
-                title: Text("CS - 4366 Senior Capstone Project"));
-          }),
+    return const Center(
+      child: Text("Student Courses Page"),
     );
   }
 }
