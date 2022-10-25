e22daaf2ec0a02eb130ed6961a2cce2cd9689908
andreamkx <82063749+andreamkx@users.noreply.github.com>
2022-10-24 19:55:33 -0500

    Update student_announcements_page.dart
    Added announcements via ListView()

--- assembli/lib/student_announcements_page.dart
+++ assembli/lib/student_announcements_page.dart
@@ -9,8 +9,53 @@ class StudentAnnouncementsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Announcements Page"),
+    return MaterialApp(
+        title: "ListView.builder",
+        theme: ThemeData(primarySwatch: Colors.green),
+        debugShowCheckedModeBanner: false,
+        // home : new ListViewBuilder(),  NO Need To Use Unnecessary New Keyword
+        home: const ListViewBuilder());
+  }
+}
+
+class ListViewBuilder extends StatelessWidget {
+  const ListViewBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const <Widget>[
+        ListTile(
+          leading: Text("(12/04)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("CS-4366"),
+        ),
+        ListTile(
+          leading: Text("(11/22)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("CS-4352"),
+        ),
+        ListTile(
+          leading: Text("(11/20)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("CS-4354"),
+        ),
+        ListTile(
+          leading: Text("(10/31)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("CS-4366"),
+        ),
+        ListTile(
+          leading: Text("(09/10)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("BA-3305"),
+        ),
+        ListTile(
+          leading: Text("(08/18)"),
+          trailing: Text("Class Cancelled"),
+          title: Text("CS-4354"),
+        ),
+      ],
     );
   }
 }
