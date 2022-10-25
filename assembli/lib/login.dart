9364c024ee82ebb7ebd50cc2f961b2c3b0175822
NicHousden <nic.housden@gmail.com>
2022-10-25 18:23:17 -0500

    File Checking for Login

--- assembli/lib/login.dart
+++ assembli/lib/login.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:assembli/student_landing.dart';
 import 'package:assembli/instructor_landing.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
 //this file creates the login state/appearance
 
@@ -11,8 +16,45 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String textFromFile = "";
+  String tmpText = "";
+  String emailIn = "";
+  String passIn = "";
+  String rNum = "";
+  String userType = "";
   // Create controller to retrive the user input
+  bool checkLogin() {
+    bool access = false;
+    var text = textFromFile.replaceAll("\n", ",");
+    //print(text);
+    var line = text.split(",");
+
+    for (int i = 0; i < line.length; i++) {
+      if (((line[i]).toLowerCase()).compareTo((emailIn.toLowerCase())) == 0) {
+        if (line[i + 1] == (passIn)) {
+          access = true;
+          rNum = line[i + 3];
+          userType = line[i + 2];
+        }
+      }
+    }
+
+    return access;
+  }
+
+  Future<String> getFileData(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
   final myController = TextEditingController();
+  final passController = TextEditingController();
+
+  void getData() async {
+    textFromFile = await _read();
+    if (textFromFile == "") {
+      textFromFile = await _read();
+    }
+  }
 
   // Clean up the controller
   @override
@@ -21,8 +63,34 @@ class _LoginState extends State<Login> {
     super.dispose();
   }
 
+  _write() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    tmpText = await getFileData("text/credentials.txt");
+    final File file = File('${directory.path}/Credentials.txt');
+    await file.writeAsString(tmpText, flush: true);
+  }
+
+  Future<String> _read() async {
+    String text = "";
+    try {
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final File file = File('${directory.path}/Credentials.txt');
+      text = await file.readAsString();
+    } catch (e) {
+      print("Couldn't read file");
+    }
+    if (text == "") {
+      _write();
+    }
+    //print(text);
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
+    //getDir();
+    //_write("Newest Email\n");
+    getData();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -53,11 +121,12 @@ class _LoginState extends State<Login> {
                     hintText: 'Enter valid ttu email id'),
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.only(
                   left: 15.0, right: 15.0, top: 15.0, bottom: 45.0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: passController,
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
@@ -77,36 +146,34 @@ class _LoginState extends State<Login> {
                         Color.fromARGB(255, 179, 194, 168))),
                 onPressed: () {
                   // Student Landing
-                  if (myController.text == "student") {
-                    //
-                    //pushAndRemoveUntil basically opens up the landing page
-                    //and removes access to the login page until some time
-                    //
+                  //print(textFromFile);
+                  emailIn = myController.text;
+                  passIn = passController.text;
+                  bool access = checkLogin();
 
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const StudentLanding();
-                        },
-                      ),
-                      (route) => false,
-                    );
+                  if (access) {
+                    if (userType == "Student") {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const StudentLanding();
+                          },
+                        ),
+                        (route) => false,
+                      );
+                    } else {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const InstructorLanding();
+                          },
+                        ),
+                        (route) => false,
+                      );
+                    }
                   }
-
-                  // Instructor Landing
-                  else if (myController.text == "instructor") {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const InstructorLanding();
-                        },
-                      ),
-                      (route) => false,
-                    );
-                  }
-
                   // User is neither student or instructor
                   // - NOT in our database/txtFile
                   else {
