e9fdaca5c3c18b4c34108090037dc04e0a872a8d
andreamkx <82063749+andreamkx@users.noreply.github.com>
2022-10-26 12:09:25 -0500

    Added Instructor Course Home and populated requests page

--- assembli/lib/instructor_courses_page.dart
+++ assembli/lib/instructor_courses_page.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
-
-//class holds basis for courses destination;
-//will work similar to landing, linking other pages/states
-//(linked from navigationBar destination in landing.dart)
+import 'package:assembli/instructor_course_home.dart';
 
 class InstructorCoursesPage extends StatelessWidget {
   const InstructorCoursesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Instructor Courses Page"),
+    return MaterialApp(
+        title: "ListView.builder",
+        theme: ThemeData(primarySwatch: Colors.green),
+        debugShowCheckedModeBanner: false,
+        // home : new ListViewBuilder(),  NO Need To Use Unnecessary New Keyword
+        home: const ListViewBuilder());
+  }
+}
+
+class ListViewBuilder extends StatelessWidget {
+  const ListViewBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: Text("CS - 4366 Senior Capstone Project"),
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const InstructorCourseHome();
+                    },
+                  ),
+                  (route) => false,
+                );
+              },
+            );
+          }),
     );
   }
 }
