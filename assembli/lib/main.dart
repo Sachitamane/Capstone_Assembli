851f722560c9d3bb5cd7a323f4f0ac8d0d35f6fa
briaokwor <briaokwor@outlook.com>
2022-10-26 10:15:33 -0500

    firestore setup
    testing setup of firestore database, installation only, no CRUD operations changed yet

--- assembli/lib/main.dart
+++ assembli/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:assembli/login.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 // Start of the app running
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Assembli());
 }
 //may change to statefulWidget, leave for now
