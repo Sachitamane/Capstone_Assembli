a3ab0e6f787fecbcb8f18dcac9692e48b0ad01b1
briaokwor <briaokwor@outlook.com>
2022-10-27 11:57:13 -0500

    testing db pulls/reads
    multiple version of a stream builder on here (from different videos). Right now the only problem is the error I keep getting
    starting with ...
    Stream closed with status: Status{code=UNAVAILABLE, description=Channel shutdownNow invoked, cause=null}
    
    search suggests it may be caused by poor internet connection, when it comes to connecting to the database.
    
    ++NOTE: changed emulator to one with Google Play store capabilities :
    utilizing Google Pixel API 29 instead of Pixel XL API 30

--- assembli/lib/student_courses_page.dart
+++ assembli/lib/student_courses_page.dart
@@ -1,11 +1,110 @@
-import 'package:flutter/material.dart';
-import 'package:assembli/student_course_home.dart';
-import 'package:firebase_core/firebase_core.dart';
 
+import 'package:flutter/material.dart';
+//import 'package:assembli/student_course_home.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 //changing pushAndRemoveUntil into push only
-//change to stateful, 
+//change to stateful, then figure out how to read from DB
+//https://www.youtube.com/watch?v=ZHdg2kfKmjI
 
+class StudentCoursesPage extends StatefulWidget {
+  const StudentCoursesPage({super.key});
+
+  @override
+  State<StudentCoursesPage> createState() => _StudentCoursesPageState();
+}
+class _StudentCoursesPageState extends State<StudentCoursesPage> {
+  final controller = TextEditingController();
+  final CollectionReference courseCollection = FirebaseFirestore.instance.collection('Course');
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      /*appBar: AppBar(
+        title: const Text('draft'),
+      ),*/
+      body: Column(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(16),
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search),
+              hintText: 'Search Courses',
+              border: OutlineInputBorder(
+                borderRadius:  BorderRadius.circular(20),
+                borderSide: const BorderSide(color: Color.fromARGB(255, 179, 194, 168))
+            ),
+          ),
+          //onChanged: //create search method here 
+        ),
+      ),
+
+      //may work with better connection or different wifi, try again later
+      StreamBuilder(
+        stream: courseCollection.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot){
+          if(streamSnapshot.hasData){
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index){
+                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                return Card(
+                  margin: const EdgeInsets.all(16),
+                  child: ListTile(
+                    title: Text(documentSnapshot['cname']),
+                    subtitle: const Text('dubbb'),
+                  ),
+                  );
+              },
+            );
+          }
+          return const Center(child: CircularProgressIndicator(),);
+        }
+      )
+      //Expanded(
+        //child: 
+    /*    StreamBuilder<List<Course>>(
+          stream: readCourses(),
+          builder: (context, snapshot){
+            if (snapshot.hasError) {
+              return const Text('Error here');
+            }
+            else if(snapshot.hasData) {
+                  final courses = snapshot.data!;
+            
+                  return ListView(
+                  children: courses.map(buildUser).toList(),
+                  );
+            } else{
+              return const Center(child: CircularProgressIndicator());
+            }
+          }
+
+         /* child: ListView.builder(
+            itemCount: 
+            itemBuilder: (context) 
+            ),*/
+        ),
+     */  // )
+      ]
+    ),
+  );
+////////////////////////////////////WHAT'S TO BE DISPLAYED IN LISTVIEW
+  Widget buildUser(Course course) => ListTile(
+    leading: CircleAvatar(child: Text(/*'${course.cname}'*/ course.cname)),
+  );
+
+
+  Stream<List<Course>> readCourses() => FirebaseFirestore.instance
+      .collection('Course')
+      .snapshots()  /*gets all documents from firebase collection*/
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Course.fromJson(doc.data())).toList());
+}
+
+/*
 class StudentCoursesPage extends StatelessWidget {
   const StudentCoursesPage({Key? key}) : super(key: key);
 
@@ -54,6 +153,7 @@ class ListViewBuilder extends StatelessWidget{
     
   }
 }
+*/
 /////////////////////////////////////////////////////////////////////////////////////////////
 class Course{
   final int rnum;         //teacher rnum
@@ -77,5 +177,13 @@ class Course{
     'rnum': rnum,
     'sect': sect,
   };
+
+  static Course fromJson(Map<String, dynamic> json) => Course(
+    cid: json['cid'],
+    cname: json['cname'],
+    crn: json['crn'],
+    rnum: json['rnum'],
+    sect: json['sect'],
+  );
 }
 
