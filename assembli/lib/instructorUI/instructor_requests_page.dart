3921995510d9b54db99a00151072bc3622ef5e5b
andreamkx <82063749+andreamkx@users.noreply.github.com>
2022-11-12 02:44:36 -0600

    Update instructor_requests_page.dart
    Clean up display and show the necessary data for pending requests only
    
    Still need approve/deny functionality

--- assembli/lib/instructorUI/instructor_requests_page.dart
+++ assembli/lib/instructorUI/instructor_requests_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// Completed: Get app to display every status that's in db
-//            Have Approve and Deny button for each
-// Not completed: Student info does not show, every request
-//                is showing, should only show pending
+// Completed: Displays all pending requests in Request collection
+//            Approve/Deny buttons for each
+
+// Not completed: Rnum needs to link to specific user
 //                Show only requests from students the professor
 //                actually teaches
+//                Make approve deny buttons functional (update db)
 
 class InstructorRequestsPage extends StatefulWidget {
   const InstructorRequestsPage({super.key});
@@ -17,8 +18,13 @@ class InstructorRequestsPage extends StatefulWidget {
 
 class _InstructorRequestsPageState extends State<InstructorRequestsPage> {
   final controller = TextEditingController();
-  CollectionReference requestCollection =
-      FirebaseFirestore.instance.collection('Request');
+  // Only show requests with status == "pending"
+  // contained in Request collection
+  //CollectionReference requests =
+  //    FirebaseFirestore.instance.collection('Request');
+  Query<Map<String, dynamic>> requestCollection = FirebaseFirestore.instance
+      .collection('Request')
+      .where("status", isEqualTo: "pending");
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -35,49 +41,83 @@ class _InstructorRequestsPageState extends State<InstructorRequestsPage> {
                         final DocumentSnapshot documentSnapshot =
                             streamSnapshot.data!.docs[index];
                         return Card(
-                          margin: const EdgeInsets.all(16),
+                          margin: const EdgeInsets.all(10),
                           child: ListTile(
-                              // Put all request information here
-                              title: Text(
-                                documentSnapshot['status'],
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              // Have approve/deny button for each
-                              //subtitle: const Text('Approve | Deny'),
+                              // Show class, cid, and rnum,
+                              // seperated by SizedBoxes to look nice
+                              title: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const Text('Class: ',
+                                        style: TextStyle(fontSize: 17)),
+                                    Text(
+                                      documentSnapshot['cid'],
+                                      style: const TextStyle(fontSize: 17),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                      height: 50,
+                                    ),
+                                    const Text('Rnum: ',
+                                        style: TextStyle(fontSize: 17)),
+                                    Text(documentSnapshot['rnum'],
+                                        style: const TextStyle(fontSize: 17)),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    const Text('Reason: ',
+                                        style: TextStyle(fontSize: 17)),
+                                    Text(documentSnapshot['reason'],
+                                        style: const TextStyle(fontSize: 17)),
+                                  ]),
+                              // Section for approve and deny
                               subtitle: Row(
                                   mainAxisSize: MainAxisSize.min,
                                   children: <Widget>[
-                                    ElevatedButton(
-                                      style: const ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStatePropertyAll<Color>(
-                                                  Color.fromARGB(
-                                                      255, 179, 194, 168))),
-                                      onPressed: () {},
-                                      // Update database to "Approved"
-                                      child: const Text(
-                                        'Approve',
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 25),
+                                    // Approve button
+                                    SizedBox(
+                                      width: 105,
+                                      height: 30,
+                                      child: ElevatedButton(
+                                        style: const ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStatePropertyAll<Color>(
+                                                    Color.fromARGB(
+                                                        255, 179, 194, 168))),
+                                        onPressed:
+                                            () {}, // Will need to add functionality to update to db
+                                        child: const Text(
+                                          'Approve',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17),
+                                        ),
                                       ),
+                                    ),
+                                    // Deny button
+                                    const SizedBox(
+                                      width: 10,
+                                      height: 37,
                                     ),
                                     SizedBox(
-                                      width: 20,
-                                    ),
-                                    ElevatedButton(
-                                      style: const ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStatePropertyAll<Color>(
-                                                  Color.fromARGB(
-                                                      255, 179, 194, 168))),
-                                      onPressed: () {},
-                                      // Update database to "Denied"
-                                      child: const Text(
-                                        'Deny',
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 25),
-                                      ),
-                                    )
+                                        height: 30,
+                                        width: 85,
+                                        child: ElevatedButton(
+                                          style: const ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStatePropertyAll<
+                                                          Color>(
+                                                      Color.fromARGB(
+                                                          255, 179, 194, 168))),
+                                          onPressed:
+                                              () {}, // Will need to add functionality to update to db
+                                          child: const Text(
+                                            'Deny',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 17),
+                                          ),
+                                        ))
                                   ])),
                         );
                       },
