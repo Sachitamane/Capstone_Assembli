767556e697364c54359d65872cf44675dc8e9959
NicHousden <nic.housden@gmail.com>
2022-11-14 17:54:34 -0600

    User is now authenticated
    Data is pulled at the build of the login page. It is stored in the User class in the User.dart file under a corresponding list. The email/password lists are checked on the login button press. If it is valid, then it moves on to the corresponding course landing page.

--- assembli/lib/login.dart
+++ assembli/lib/login.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:assembli/studentUI/student_landing.dart';
 import 'package:assembli/instructorUI/instructor_landing.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:assembli/user.dart';
 
 //this file creates the login state/appearance
 
@@ -14,10 +17,25 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   // Create controller to retrive the user input
   final myController = TextEditingController();
+  final passControllers = TextEditingController();
   final CollectionReference courseCollection =
       FirebaseFirestore.instance.collection('Users');
-
+  bool isValid = false;
+  String email = "";
+  String pass = "";
   // Clean up the controller
+  void validateUser() {
+    for (int i = 0; i < User.emails.length; i++) {
+      if (User.emails[i] == email) {
+        if (User.passwords[i] == pass) {
+          isValid = true;
+          User.type = User.types[i];
+          User.rnum = User.rNums[i];
+        }
+      }
+    }
+  }
+
   @override
   void dispose() {
     myController.dispose();
@@ -56,12 +74,32 @@ class _LoginState extends State<Login> {
                     hintText: 'Enter valid ttu email id'),
               ),
             ),
-            const Padding(
+            StreamBuilder(
+                stream: courseCollection.snapshots(),
+                builder:
+                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                  if (streamSnapshot.hasData) {
+                    int index = streamSnapshot.data!.docs.length;
+                    if (User.emails.isEmpty) {
+                      for (int i = 0; i < index; i++) {
+                        final DocumentSnapshot documentSnapshot =
+                            streamSnapshot.data!.docs[i];
+                        User.emails.add(documentSnapshot['email']);
+                        User.passwords.add(documentSnapshot['password']);
+                        User.rNums.add(documentSnapshot['rnum']);
+                        User.types.add(documentSnapshot['type']);
+                      }
+                    }
+                  }
+                  return const Center();
+                }),
+            Padding(
               padding: EdgeInsets.only(
                   left: 15.0, right: 15.0, top: 15.0, bottom: 45.0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
                 obscureText: true,
+                controller: passControllers,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Password',
@@ -80,39 +118,44 @@ class _LoginState extends State<Login> {
                         Color.fromARGB(255, 179, 194, 168))),
                 onPressed: () {
                   // Student Landing
-                  if (myController.text == "student") {
-                    //
-                    //pushAndRemoveUntil basically opens up the landing page
-                    //and removes access to the login page until some time
-                    //
+                  print(User.emails);
+                  email = myController.text;
+                  pass = passControllers.text;
+                  validateUser();
+                  if (isValid) {
+                    if (User.type == "student") {
+                      //
+                      //pushAndRemoveUntil basically opens up the landing page
+                      //and removes access to the login page until some time
+                      //
 
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const StudentLanding();
-                        },
-                      ),
-                      (route) => false,
-                    );
-                  }
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const StudentLanding();
+                          },
+                        ),
+                        (route) => false,
+                      );
+                    }
 
-                  // Instructor Landing
-                  else if (myController.text == "instructor") {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const InstructorLanding();
-                        },
-                      ),
-                      (route) => false,
-                    );
-                  }
+                    // Instructor Landing
+                    else if (User.type == "instructor") {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const InstructorLanding();
+                          },
+                        ),
+                        (route) => false,
+                      );
+                    }
 
-                  // User is neither student or instructor
-                  // - NOT in our database/txtFile
-                  else {
+                    // User is neither student or instructor
+                    // - NOT in our database/txtFile
+                  } else {
                     Navigator.pushAndRemoveUntil(
                       context,
                       MaterialPageRoute(
---
+++ assembli/lib/user.dart
@@ -0,0 +1,8 @@
+class User {
+  static String rnum = "";
+  static List<String> emails = [];
+  static List<String> passwords = [];
+  static List<String> rNums = [];
+  static List<String> types = [];
+  static String type = "";
+}
