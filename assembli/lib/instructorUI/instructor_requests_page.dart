0dba71e5342c1ddadce9eda488b4a9e7730a0a10
andreamkx <82063749+andreamkx@users.noreply.github.com>
2022-11-18 01:06:30 -0600

    Update instructor_requests_page.dart
    formatting

--- assembli/lib/instructorUI/instructor_requests_page.dart
+++ assembli/lib/instructorUI/instructor_requests_page.dart
@@ -20,108 +20,118 @@ class _InstructorRequestsPageState extends State<InstructorRequestsPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-          body: Column(children: <Widget>[
-        Expanded(
-          child: StreamBuilder(
-            stream: requestCollection.snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-              if (streamSnapshot.hasData) {
-                // Create ListView with pending requests
-                return ListView.builder(
-                  itemCount: streamSnapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    final DocumentSnapshot documentSnapshot =
-                        streamSnapshot.data!.docs[index];
-                    return Card(
-                      margin: const EdgeInsets.all(10),
-                      child: ListTile(
-                        // title: show student request information
-                        title: Row(mainAxisSize: MainAxisSize.min, children: <
-                            Widget>[
-                          // Display cid
-                          const Text('Class: ', style: TextStyle(fontSize: 17)),
-                          Text(
-                            documentSnapshot['cid'],
-                            style: const TextStyle(fontSize: 17),
-                          ),
-                          const SizedBox(width: 10, height: 50),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder(
+                stream: requestCollection.snapshots(),
+                builder:
+                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                  if (streamSnapshot.hasData) {
+                    // Create ListView with pending requests
+                    return ListView.builder(
+                      itemCount: streamSnapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot documentSnapshot =
+                            streamSnapshot.data!.docs[index];
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            // title: show student request information
+                            title: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  // Display cid
+                                  const Text('Class: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(
+                                    documentSnapshot['cid'],
+                                    style: const TextStyle(fontSize: 17),
+                                  ),
+                                  const SizedBox(width: 10, height: 50),
 
-                          // Display rnum
-                          const Text('Rnum: ', style: TextStyle(fontSize: 17)),
-                          Text(documentSnapshot['rnum'],
-                              style: const TextStyle(fontSize: 17)),
-                          const SizedBox(width: 10),
+                                  // Display rnum
+                                  const Text('Rnum: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(documentSnapshot['rnum'],
+                                      style: const TextStyle(fontSize: 17)),
+                                  const SizedBox(width: 10),
 
-                          // Display reason
-                          const Text('Reason: ',
-                              style: TextStyle(fontSize: 17)),
-                          Text(documentSnapshot['reason'],
-                              style: const TextStyle(fontSize: 17)),
-                        ]),
+                                  // Display reason
+                                  const Text('Reason: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(documentSnapshot['reason'],
+                                      style: const TextStyle(fontSize: 17)),
+                                ]),
 
-                        // subtitle: row with approve/deny buttons
-                        subtitle:
-                            Row(mainAxisSize: MainAxisSize.min, children: <
-                                Widget>[
-                          // Approve button
-                          SizedBox(
-                            width: 105,
-                            height: 30,
-                            child: ElevatedButton(
-                              style: const ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll<Color>(
-                                          Color.fromARGB(255, 179, 194, 168))),
-                              onPressed: () {
-                                // Update status in db
-                                FirebaseFirestore.instance
-                                    .collection('Request')
-                                    .doc('0o4dfGlW1quxU2Axmlt8')
-                                    .update({'status': 'approved'});
-                              },
-                              child: const Text(
-                                'Approve',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 17),
-                              ),
+                            // subtitle: row with approve/deny buttons
+                            subtitle: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                // Approve button
+                                SizedBox(
+                                  width: 105,
+                                  height: 30,
+                                  child: ElevatedButton(
+                                    style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll<Color>(
+                                                Color.fromARGB(
+                                                    255, 179, 194, 168))),
+                                    onPressed: () {
+                                      // Update status in db
+                                      FirebaseFirestore.instance
+                                          .collection('Request')
+                                          .doc('0o4dfGlW1quxU2Axmlt8')
+                                          .update({'status': 'approved'});
+                                    },
+                                    child: const Text(
+                                      'Approve',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 17),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10, height: 37),
+
+                                // Deny button
+                                SizedBox(
+                                  height: 30,
+                                  width: 85,
+                                  child: ElevatedButton(
+                                    style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll<Color>(
+                                                Color.fromARGB(
+                                                    255, 179, 194, 168))),
+                                    onPressed: () {
+                                      // Update status in db
+                                      FirebaseFirestore.instance
+                                          .collection('Request')
+                                          .doc('0o4dfGlW1quxU2Axmlt8')
+                                          .update({'status': 'denied'});
+                                    },
+                                    child: const Text(
+                                      'Deny',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 17),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(width: 10, height: 37),
-
-                          // Deny button
-                          SizedBox(
-                            height: 30,
-                            width: 85,
-                            child: ElevatedButton(
-                              style: const ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll<Color>(
-                                          Color.fromARGB(255, 179, 194, 168))),
-                              onPressed: () {
-                                // Update status in db
-                                FirebaseFirestore.instance
-                                    .collection('Request')
-                                    .doc('0o4dfGlW1quxU2Axmlt8')
-                                    .update({'status': 'denied'});
-                              },
-                              child: const Text(
-                                'Deny',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 17),
-                              ),
-                            ),
-                          ),
-                        ]),
-                      ),
+                        );
+                      },
                     );
-                  },
-                );
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-        )
-      ]));
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
 }
