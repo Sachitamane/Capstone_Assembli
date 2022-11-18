e7493f7efc29d878d22f9c5411b7a92789bd11de
Sachitamane <82063804+Sachitamane@users.noreply.github.com>
2022-11-18 13:31:35 -0600

    updated instructor UI

--- assembli/lib/instructorUI/instructor_course_home.dart
+++ assembli/lib/instructorUI/instructor_course_home.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:assembli/location.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class InstructorCourseHome extends StatelessWidget {
-  const InstructorCourseHome({Key? key}) : super(key: key);
+class InstructorCourseHome extends StatefulWidget {
+  final String courseName;
+  final int courseNumb;
+
+  const InstructorCourseHome({
+    Key? key,
+    required this.courseName,
+    required this.courseNumb,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
-    return Center(
-        child: ElevatedButton(
-      style: const ButtonStyle(
-          backgroundColor: MaterialStatePropertyAll<Color>(
-              Color.fromARGB(255, 179, 194, 168))),
-      onPressed: () {
-            
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const LocationSetter();
-                        },
-                      ),
-                      (route) => false,
-                    );
-       },
-      child: const Text('Open Attendance',
-          style: TextStyle(color: Colors.white, fontSize: 25)),
-    ));
-  }
+  State<InstructorCourseHome> createState() => _InstructorCourseHomeState();
+}
+
+class _InstructorCourseHomeState extends State<InstructorCourseHome> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 179, 194, 168),
+        title: Text(widget.courseName),
+      ),
+      body: ElevatedButton(
+        style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll<Color>(
+                Color.fromARGB(255, 179, 194, 168)),
+            fixedSize: MaterialStatePropertyAll(Size(400, 20))),
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const LocationSetter();
+              },
+            ),
+            (route) => false,
+          );
+        },
+        child: const Text('Open Attendance',
+            style: TextStyle(color: Colors.white, fontSize: 25)),
+      ));
 }
--- assembli/lib/location.dart
+++ assembli/lib/location.dart
@@ -6,7 +6,7 @@ import 'package:assembli/studentUI/student_course_home.dart';
 import 'package:simple_timer/simple_timer.dart';
 import 'package:assembli/instructorUI/instructor_course_home.dart';
 
-//references to student course home in this file will need to be 
+//references to student course home in this file will need to be
 //updated with constructor requirements
 
 /*
@@ -21,7 +21,7 @@ class LocationFinder extends StatefulWidget {
   State<LocationFinder> createState() => _LocationFinderState();
 }
 
-class _LocationFinderState extends State<LocationFinder> {  
+class _LocationFinderState extends State<LocationFinder> {
   //for holding gps location of student
   var _latitude = "";
   var _longitude = "";
@@ -33,8 +33,7 @@ class _LocationFinderState extends State<LocationFinder> {
   bool _cancheck = false;
   //hard coded positions of Instructor
   double instructorLatitude = 33.5855774;
-  double instructorLongitude = -101.8737846; 
-
+  double instructorLongitude = -101.8737846;
 
   //used to compare Instructors position and Students Position and check Students position validness
   //updates _valid and _cancheck boolean variables
@@ -55,17 +54,15 @@ class _LocationFinderState extends State<LocationFinder> {
     });
   }
 
-
   //used to update the _latitude and _longitude; Student GPS Location
   Future<void> _updatePosition() async {
     Position pos = await _determinePosition();
-   // List pm = await placemarkFromCoordinates(pos.latitude, pos.longitude);
+    // List pm = await placemarkFromCoordinates(pos.latitude, pos.longitude);
     setState(() {
       _latitude = pos.latitude.toString();
       _longitude = pos.longitude.toString();
     });
   }
-
 
   /// Determine the current position of the device.
   ///
@@ -114,79 +111,111 @@ class _LocationFinderState extends State<LocationFinder> {
     _updatePosition();
     _checkPosition();
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: Center(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Center(
               child: Column(
-                children: <Widget>[
-                    //empty container for aesthetics
-                    Container(
-                        height: 50,
-                          width: 250,
-                      ),
-                      Container(
-                        height: 100,
-                          width: 250,
-                          decoration: BoxDecoration(
-                                        border: Border.all(color: Color.fromARGB(255, 179, 194, 168)),
-                                        borderRadius: BorderRadius.circular(20)
-                                    ), 
-                        child: Center( child: !_cancheck?
-                                                  Text('Proximity Check: Pending', style: TextStyle(fontSize: 18)):
-                                                  _valid?Text('Proximity Check: Valid', style: TextStyle(fontSize: 18,color: Colors.green)):
-                                                              Text('Proximity Check: Invalid', style: TextStyle(fontSize: 18,color: Colors.red)),
-                                                              
-                                    )   
-                      ),
-                      Padding(
-                            padding: EdgeInsets.all(30),
-                            child: _cancheck? Column(
-                                  children: <Widget> [
-                                    const Text ('Classroom Location:',
-                                    style:  TextStyle(fontSize:20),),
-                                    Text('Latitude : ${instructorLatitude.toString()}',
-                                        style: TextStyle(fontSize:20)),
-                                    Text('Longitude : ${instructorLongitude.toString()}',
-                                        style: TextStyle(fontSize:20)),
-                                    const Text('Your Location:',
-                                        style: TextStyle(fontSize:20)),
-                                    Text('Latitude : $_latitude',
-                                        style: TextStyle(fontSize:20)),
-                                    Text('Longitude : $_longitude',
-                                        style: TextStyle(fontSize:20)),
-                                    Text('Difference : $_difference',
-                                    style: TextStyle(fontSize:20)),
-                                  ]):Center(child: CircularProgressIndicator()))
-                        ,
-                                  //if valid displays inputfield Widget to enter code and submit button to return to student course home
-                                  //, if fails displays simple timer widget and returns to student course home page
-                                  Padding(
-                                    padding: EdgeInsets.all(30),
-                                    child: Container(
-                                              height:200,
-                                              width: 250,
-                                              child: !_cancheck?
-                                              SizedBox.shrink():
-                                                    _valid?Column(children: <Widget>[ TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'Enter Code'),), ElevatedButton(child:Text('Submit Code'),onPressed: () {Navigator.pushAndRemoveUntil(
-                                                              context,
-                                                              MaterialPageRoute(
-                                                                builder: (BuildContext context) {
-                                                                  return const StudentCourseHome(
-                                                                    courseName: 'buffer',
-                                                                    courseNumb: 1111,
-                                                                  );
-                                                                },
-                                                              ),
-                                                              (route) => false,
-                                                            );}), ]):
-                                                            SimpleTimer(status: TimerStatus.start,duration: Duration(seconds: 10),onEnd: () => Navigator.pushAndRemoveUntil(
-                                                            context,MaterialPageRoute(builder: (BuildContext context) {return StudentCourseHome(courseName: 'buffer', courseNumb: 1111,);},),(route) => false,), ),
-                                              ),
-                                    ),
-                  ],
-              )),
-      )
-    );
+            children: <Widget>[
+              //empty container for aesthetics
+              Container(
+                height: 50,
+                width: 250,
+              ),
+              Container(
+                  height: 100,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      border:
+                          Border.all(color: Color.fromARGB(255, 179, 194, 168)),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: !_cancheck
+                        ? Text('Proximity Check: Pending',
+                            style: TextStyle(fontSize: 18))
+                        : _valid
+                            ? Text('Proximity Check: Valid',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.green))
+                            : Text('Proximity Check: Invalid',
+                                style:
+                                    TextStyle(fontSize: 18, color: Colors.red)),
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(30),
+                  child: _cancheck
+                      ? Column(children: <Widget>[
+                          const Text(
+                            'Classroom Location:',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text('Latitude : ${instructorLatitude.toString()}',
+                              style: TextStyle(fontSize: 20)),
+                          Text('Longitude : ${instructorLongitude.toString()}',
+                              style: TextStyle(fontSize: 20)),
+                          const Text('Your Location:',
+                              style: TextStyle(fontSize: 20)),
+                          Text('Latitude : $_latitude',
+                              style: TextStyle(fontSize: 20)),
+                          Text('Longitude : $_longitude',
+                              style: TextStyle(fontSize: 20)),
+                          Text('Difference : $_difference',
+                              style: TextStyle(fontSize: 20)),
+                        ])
+                      : Center(child: CircularProgressIndicator())),
+              //if valid displays inputfield Widget to enter code and submit button to return to student course home
+              //, if fails displays simple timer widget and returns to student course home page
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: Container(
+                  height: 200,
+                  width: 250,
+                  child: !_cancheck
+                      ? SizedBox.shrink()
+                      : _valid
+                          ? Column(children: <Widget>[
+                              TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter Code'),
+                              ),
+                              ElevatedButton(
+                                  child: Text('Submit Code'),
+                                  onPressed: () {
+                                    Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (BuildContext context) {
+                                          return const StudentCourseHome(
+                                            courseName: 'buffer',
+                                            courseNumb: 1111,
+                                          );
+                                        },
+                                      ),
+                                      (route) => false,
+                                    );
+                                  }),
+                            ])
+                          : SimpleTimer(
+                              status: TimerStatus.start,
+                              duration: Duration(seconds: 10),
+                              onEnd: () => Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                    return StudentCourseHome(
+                                      courseName: 'buffer',
+                                      courseNumb: 1111,
+                                    );
+                                  },
+                                ),
+                                (route) => false,
+                              ),
+                            ),
+                ),
+              ),
+            ],
+          )),
+        ));
   }
 }
 
@@ -202,9 +231,7 @@ class LocationSetter extends StatefulWidget {
   State<LocationSetter> createState() => _LocationSetterState();
 }
 
-
-class _LocationSetterState extends State<LocationSetter>{
-
+class _LocationSetterState extends State<LocationSetter> {
   //for holding gps location of Instructor
   var _latitude = "";
   var _longitude = "";
@@ -215,11 +242,10 @@ class _LocationSetterState extends State<LocationSetter>{
   //to check whether the Instructor has confirmed location
   bool _confirmedLocation = false;
 
-
   //used to update _confirmedLocation to activate code inputfield
   Future<void> _instructorCodeActivation() async {
     setState(() {
-        _confirmedLocation = true;
+      _confirmedLocation = true;
     });
   }
 
@@ -230,7 +256,7 @@ class _LocationSetterState extends State<LocationSetter>{
     setState(() {
       _latitude = pos.latitude.toString();
       _longitude = pos.longitude.toString();
-      _address= pm[0].toString();
+      _address = pm[0].toString();
       _canConfirm = true;
     });
   }
@@ -277,73 +303,77 @@ class _LocationSetterState extends State<LocationSetter>{
         desiredAccuracy: LocationAccuracy.high);
   }
 
-    @override
+  @override
   Widget build(BuildContext context) {
     _updatePosition();
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: Center(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Center(
               child: Column(
-                children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.all(30),
-                            child: 
-                                const Text('Classroom Location: ',
-                                style: TextStyle(fontSize: 20)),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.all(15),
-                        child: !_canConfirm?
-                                Center(child: CircularProgressIndicator()):
-                                Column(
-                                  children: <Widget> [
-                                    Text('Latitude $_latitude',
-                                      style: TextStyle(fontSize:20)
-                                    ),
-                                    Text('Longitude $_longitude',
-                                      style: TextStyle(fontSize:20)
-                                    ),
-                                  ]
-                                ),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.all(30),
-                        child: Column(
-                          children: <Widget> [
-                            Text(_address,
-                              style: TextStyle(fontSize:15)
-                            ),
-                          ]
-                        ),
-                    ),
-                    Padding(
-                          padding: EdgeInsets.all(20),
-                          child: !_canConfirm ? 
-                                  SizedBox.shrink():
-                                    !_confirmedLocation ? Column(children: <Widget>[ElevatedButton(
-                                                                child:Text('Confirm Classroom Location'),onPressed: () 
-                                                                {
-                                                                  _instructorCodeActivation();
-                                                                }
-                                                            ), 
-                                            ]
-                                  ): Column(children:<Widget>[ TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'Input Code Login')),
-                                  ElevatedButton(onPressed: (){
-                                              Navigator.pushAndRemoveUntil(
-                                                                    context,
-                                                                    MaterialPageRoute(
-                                                                      builder: (BuildContext context) {
-                                                                        return const InstructorCourseHome();
-                                                                      },
-                                                                    ),
-                                                                    (route) => false
-                                                                  ,);
-                                  }, child:  const Text('Confirm Code',
-          style: TextStyle(color: Colors.white, fontSize: 25)),)]),
-              ),],
-              )),
-      )
-    );
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: const Text('Classroom Location: ',
+                    style: TextStyle(fontSize: 20)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: !_canConfirm
+                    ? Center(child: CircularProgressIndicator())
+                    : Column(children: <Widget>[
+                        Text('Latitude $_latitude',
+                            style: TextStyle(fontSize: 20)),
+                        Text('Longitude $_longitude',
+                            style: TextStyle(fontSize: 20)),
+                      ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(children: <Widget>[
+                  Text(_address, style: TextStyle(fontSize: 15)),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: !_canConfirm
+                    ? SizedBox.shrink()
+                    : !_confirmedLocation
+                        ? Column(children: <Widget>[
+                            ElevatedButton(
+                                child: Text('Confirm Classroom Location'),
+                                onPressed: () {
+                                  _instructorCodeActivation();
+                                }),
+                          ])
+                        : Column(children: <Widget>[
+                            TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Input Code Login')),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) {
+                                      return const InstructorCourseHome(
+                                        courseName: 'buffer',
+                                        courseNumb: 1,
+                                      );
+                                    },
+                                  ),
+                                  (route) => false,
+                                );
+                              },
+                              child: const Text('Confirm Code',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 25)),
+                            )
+                          ]),
+              ),
+            ],
+          )),
+        ));
   }
 }
