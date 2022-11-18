0a4eaad6c4138d28245d9ab4cd7ecdce29dda2df
andreamkx <82063749+andreamkx@users.noreply.github.com>
2022-11-18 13:48:32 -0600

    Update instructor_requests_page.dart
    buttons work!

--- assembli/lib/instructorUI/instructor_requests_page.dart
+++ assembli/lib/instructorUI/instructor_requests_page.dart
@@ -1,61 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-//class holds basis for announcements destination;
-//will work similar to landing, linking other pages/states
-//(linked from navigationBar destination in landing.dart)
+// Displays all pending requests in "Request" collection
+// Can be approved or denied, db will update accordingly
 
-class InstructorRequestsPage extends StatelessWidget {
-  const InstructorRequestsPage({Key? key}) : super(key: key);
+class InstructorRequestsPage extends StatefulWidget {
+  const InstructorRequestsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "ListView.builder",
-        theme: ThemeData(primarySwatch: Colors.green),
-        debugShowCheckedModeBanner: false,
-        // home : new ListViewBuilder(),  NO Need To Use Unnecessary New Keyword
-        home: const ListViewBuilder());
-  }
+  State<InstructorRequestsPage> createState() => _InstructorRequestsPageState();
 }
 
-class ListViewBuilder extends StatelessWidget {
-  const ListViewBuilder({Key? key}) : super(key: key);
+class _InstructorRequestsPageState extends State<InstructorRequestsPage> {
+  final controller = TextEditingController();
+  Query<Map<String, dynamic>> requestCollection = FirebaseFirestore.instance
+      .collection('Request')
+      .where("status", isEqualTo: "pending"); // Only get pending requests
 
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        ListTile(
-          leading: Text("(12/04)"),
-          trailing: Text("Pending"),
-          title: Text("John Doe"),
+  Widget build(BuildContext context) => Scaffold(
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder(
+                stream: requestCollection.snapshots(),
+                builder:
+                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                  if (streamSnapshot.hasData) {
+                    // Create ListView with pending requests
+                    return ListView.builder(
+                      itemCount: streamSnapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot documentSnapshot =
+                            streamSnapshot.data!.docs[index];
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            // title: row showing student request information
+                            title: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  // Display cid
+                                  const Text('Class: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(
+                                    documentSnapshot['cid'],
+                                    style: const TextStyle(fontSize: 17),
+                                  ),
+                                  const SizedBox(width: 10, height: 50),
+
+                                  // Display rnum
+                                  const Text('Rnum: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(documentSnapshot['rnum'],
+                                      style: const TextStyle(fontSize: 17)),
+                                  const SizedBox(width: 10),
+
+                                  // Display reason
+                                  const Text('Reason: ',
+                                      style: TextStyle(fontSize: 17)),
+                                  Text(documentSnapshot['reason'],
+                                      style: const TextStyle(fontSize: 17)),
+                                ]),
+
+                            // subtitle: row with approve/deny buttons
+                            subtitle: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                // Approve button
+                                SizedBox(
+                                  width: 105,
+                                  height: 30,
+                                  child: ElevatedButton(
+                                    style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll<Color>(
+                                                Color.fromARGB(
+                                                    255, 179, 194, 168))),
+                                    onPressed: () {
+                                      // Update status in db for specific document
+                                      documentSnapshot.reference
+                                          .update({'status': 'approved'});
+                                    },
+                                    child: const Text(
+                                      'Approve',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 17),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10, height: 37),
+
+                                // Deny button
+                                SizedBox(
+                                  height: 30,
+                                  width: 85,
+                                  child: ElevatedButton(
+                                    style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll<Color>(
+                                                Color.fromARGB(
+                                                    255, 179, 194, 168))),
+                                    onPressed: () {
+                                      // Update status in db for specific document
+                                      documentSnapshot.reference
+                                          .update({'status': 'denied'});
+                                    },
+                                    child: const Text(
+                                      'Deny',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 17),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        ListTile(
-          leading: Text("(11/22)"),
-          trailing: Text("Pending"),
-          title: Text("Kimberly Fisher"),
-        ),
-        ListTile(
-          leading: Text("(11/20)"),
-          trailing: Text("Pending"),
-          title: Text("Alyssa Nooney"),
-        ),
-        ListTile(
-          leading: Text("(10/31)"),
-          trailing: Text("Pending"),
-          title: Text("Eric Wright"),
-        ),
-        ListTile(
-          leading: Text("(09/10)"),
-          trailing: Text("Pending"),
-          title: Text("Erica Fuller"),
-        ),
-        ListTile(
-          leading: Text("(08/18)"),
-          trailing: Text("Pending"),
-          title: Text("George Brown"),
-        ),
-      ],
-    );
-  }
+      );
 }
