942e6999a86058b7af58365e3b9cbea968a3fa1b
briaokwor <briaokwor@outlook.com>
2022-11-27 11:50:54 -0600

    stu course home ui match
    match ui to sachitam branch on stu course home side

--- assembli/lib/studentUI/student_course_home.dart
+++ assembli/lib/studentUI/student_course_home.dart
@@ -1,6 +1,7 @@
 import 'package:assembli/location.dart';
 import 'package:assembli/models/attendance_model.dart';
 import 'package:assembli/models/course_model.dart';
+import 'package:assembli/studentUI/student_announcements_page.dart';
 import 'package:assembli/studentUI/student_course_attendance.dart';
 import 'package:assembli/studentUI/student_create_request.dart';
 import 'package:flutter/material.dart';
@@ -27,46 +28,108 @@ class _StudentCourseHomeState extends State<StudentCourseHome> {
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
       backgroundColor: const Color.fromARGB(255, 179, 194, 168),
-      title: Text("CS - ${widget.course.cid} ${widget.course.cname}"),
-
+      //title: Text("CS - ${widget.course.cid} ${widget.course.cname}"),
+      title: const Text('Assembli'),
     ),
-
+    resizeToAvoidBottomInset: false,
     body: Column(children: <Widget>[
+      //sized box to display the selected course
+      const SizedBox(height: 35),
+      Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 10),
+          child: DefaultTextStyle(
+            style: const TextStyle(
+              color: Color.fromARGB(255, 123, 123, 123),
+              fontSize: 20,
+              fontStyle: FontStyle.italic),
+              child: Text("CS - ${widget.course.cid} ${widget.course.cname}"),
+            ),
+      ),
+      
+      //divider
+      const Padding(
+        padding: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+        child: Divider(
+          height: 20,
+          thickness: 2,
+          color: Color.fromARGB(255, 243, 113, 113),
+        ),
+      ),
+      
+      //row widget
       Padding(
         padding: const EdgeInsets.all(12.0),
         child: Row(
           children:  <Widget>[
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                margin: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: const Color.fromARGB(255, 179, 194, 168)),
-                  borderRadius: const BorderRadius.all( Radius.circular(8)),
-                ),
-                child: Text('${globals.runningUser!.attends!.where((element) => element.present == true)
-                                                   .where((element) => element.crn == widget.course.crn)
-                                                   .length} / ${globals.runningUser!.attends!
-                                                   .where((element) => element.crn == widget.course.crn).length}')
-              ),
+          
+            //attendance ratio
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 7, 10, 7),
+              margin: const EdgeInsets.all(20),
+             /* decoration: BoxDecoration(
+                border: Border.all(width: 1, color: const Color.fromARGB(255, 179, 194, 168)),
+                borderRadius: const BorderRadius.all( Radius.circular(8)),
+              ),*/
+              child: Text('${globals.runningUser!.attends!.where((element) => element.present == true)
+                                                 .where((element) => element.crn == widget.course.crn)
+                                                 .length} / ${globals.runningUser!.attends!
+                                                 .where((element) => element.crn == widget.course.crn).length}',
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 91, 90, 90),
+                            fontSize: 20
+                          )
+              )
             ),
-
-             Expanded(
-              child:  TextButton(
+          
+            //attendance score button
+            Expanded(
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(
+                    Color.fromARGB(255, 255, 255, 255))
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>  StudentCourseAttendance( 
+                          attenList: globals.runningUser!.attends!
+                          .where((element) => element.crn == widget.course.crn)
+                          .toList()
+                        )
+                      ),
+                    );
+                  },
+                child: const Text('Attendance Score',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 91, 90, 90),
+                        fontSize: 20
+                      )
+                ),
+              )
+              /*child:  TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) =>  StudentCourseAttendance( attenList: globals.runningUser!.attends!.where((element) => element.crn == widget.course.crn).toList(),)),
+                      builder: (context) =>  StudentCourseAttendance( 
+                        attenList: globals.runningUser!.attends!
+                        .where((element) => element.crn == widget.course.crn)
+                        .toList()
+                      )
+                    ),
                   );
                 },
                 child: const Text('Your Attendance Score')
-                
-              ), 
+              ), */
             )
+          
           ]
         )
       ),
+
+      //log my attendance button
       ElevatedButton(
         style: const ButtonStyle(
           backgroundColor: MaterialStatePropertyAll<Color>(
@@ -83,7 +146,48 @@ class _StudentCourseHomeState extends State<StudentCourseHome> {
           child: const Text('Log my Attendance',
               style: TextStyle(color: Colors.white, fontSize: 25)),
         ),
-        ElevatedButton(
+
+      //divider
+      const Padding(
+        padding:  EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+        child: Divider(
+          height: 20,
+          thickness: 2,
+          color: Color.fromARGB(255, 243, 113, 113),
+        ),
+      ),
+
+      //see announcements button  
+      SizedBox(
+        width: 370,
+        height: 33,
+        child: ElevatedButton(
+          style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(
+                  Color.fromARGB(255, 179, 194, 168))),
+          onPressed: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return const StudentAnnouncementsPage();
+                },
+              ),
+              (route) => false,
+            );
+          },
+          child: const Text('Announcements',
+              style: TextStyle(color: Colors.white, fontSize: 25)),
+        ),
+      ),
+      
+      //spacer
+      const SizedBox(height: 35),
+      //create request button  
+      SizedBox(
+        width: 370,
+        height: 33,
+        child: ElevatedButton(
           style: const ButtonStyle(
               backgroundColor: MaterialStatePropertyAll<Color>(
                   Color.fromARGB(255, 179, 194, 168))),
@@ -100,36 +204,9 @@ class _StudentCourseHomeState extends State<StudentCourseHome> {
           },
           child: const Text('Request Excused Absence',
               style: TextStyle(color: Colors.white, fontSize: 25)),
-        ),
-      ]));
+          ),
+      ),
+    ])
+  );
 
 }
-//old StudentCourseHomeState
-/*
-class _StudentCourseHomeState extends State<StudentCourseHome> {
-  //const StudentCourseHome({Key? key}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: ElevatedButton(
-      style: const ButtonStyle(
-          backgroundColor: MaterialStatePropertyAll<Color>(
-              Color.fromARGB(255, 179, 194, 168))),
-      onPressed: () {
-            Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const LocationFinder();
-                    },
-                  ),
-                  (route) => false,
-                );
-      },
-      child: const Text('Log my Attendance',
-          style: TextStyle(color: Colors.white, fontSize: 25)),
-    ));
-  }
-}
-*/
